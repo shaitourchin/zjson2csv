5df25d41f4d9e789b8826a8220f4952fcf82f763
Shai Tourchin <shai.tourchin@neokarm.com>
2021-12-05 15:49:18 +0200

    add more fields

--- zjson2csv.py
+++ zjson2csv.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python
-# Python program to convert JSON file to CSV
+# Python program to convert openflow dump from JSON file to CSV
+#
+# usage:
+# step1: flozer --json < flows_dump.txt > flows.json
+# step2: zjson2csv < data.json
 
 
 import json
@@ -22,8 +26,11 @@ headers = [
 	'table',
 	'priority',
 	'matches',
+	'matches-protocol',
+	'matches-reg5',
+	'matches-reg6',
 	'actions',
-	'resubmit-table'
+	'actions-resubmit-table'
 ]
 csv_writer.writerow(headers)
 
@@ -32,14 +39,27 @@ for input_row in input_data:
 	if any(['icmp6' in match or 'udp6' in match or 'ipv6' in match for match in matches]):
 		continue
 
-	resubmit_table = ''
+	matches_protocol = ''
+	matches_reg5 = ''
+	matches_reg6 = ''
+	matches = input_row['matches']
+	protocols = ['arp', 'tcp', 'udp', 'icmp', 'ip']
+	for match in list(matches):
+		for match_key, match_value in match.viewitems():
+			if match_key == 'reg5':
+				matches_reg5 = match_value
+			if match_key == 'reg6':
+				matches_reg6 = match_value
+			if match_key in protocols:
+				matches_protocol = match_key
+
+	actions_resubmit_table = ''
 	actions = input_row['actions']
 	for action in list(actions):
 		for action_key in action.viewkeys():
 			re_result = re.match('^resubmit\(,(\d+)', action_key)
 			if re_result:
-				resubmit_table = re_result.group(1)
-				actions.remove(action)
+				actions_resubmit_table = re_result.group(1)
 				break
 
 	output_row = [
@@ -47,8 +67,11 @@ for input_row in input_data:
 		input_row['table'],
 		input_row['priority'],
 		matches,
+		matches_protocol,
+		matches_reg5,
+		matches_reg6,
 		actions,
-		resubmit_table
+		actions_resubmit_table
 	]
 	csv_writer.writerow(output_row)
 
